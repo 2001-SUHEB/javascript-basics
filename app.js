1d43dc5f0a887d8d0fbc48e9f027e5af1ffacbc3
2001-SUHEB <syedsha.suheb2020@vitstudent.ac.in>
2024-03-14 22:02:27 +0530

    learned primitive datatypes

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+
+//basics
 const myname="suheb"
 
 let a=5
 
-console.log(myname)
+console.log(myname)  //cannot be changed
 console.log(a)
 
 a=999
 
 console.log(a)
+
+//datatypes
+let mynum;
+console.log(mynum)
+
+let boolvalue=true
+let nullvalue=null
+
+console.log(boolvalue)
+console.log(nullvalue)
