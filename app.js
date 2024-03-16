f0a30c69f9c4b6c67a294d0b81f41f6590e8ea7c
syed sha suheb <syedsha.suheb2020@vitstudent.ac.in>
2024-03-16 21:49:52 +0530

    composite datatype(map)

--- app.js
+++ app.js
@@ -23,9 +23,33 @@
 
 //array
 
-let array=[1,2,3,4,5,6,7,8]
+// let array=[1,2,3,4,5,6,7,8]
 
-console.log(array.length)
-console.log(array[0])
-console.log(array[1])
-console.log(array[array.length-1])
+// console.log(array.length)
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[array.length-1])
+
+//composite data type--(map)
+
+const person={
+    name:{
+        firstname:"syed",
+        middlename:"sha",
+        lastname:"suheb"
+    },
+    age:22,
+    city:"Vellore"
+
+}
+console.log(person)
+
+const student={
+    name:"syed sha suheb",
+    age:22,
+    courses:[{coursname:"DSA",coursecredits:3},{coursname:"web dev",coursecredits:4}]
+}
+console.log(student)
+console.log(student.courses)
+console.log(student.courses[0])
+console.log(student.courses[1].coursname)
