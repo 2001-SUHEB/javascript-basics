5d3d926bf143f7152e512353e76c992a4ad77a01
syed sha suheb <syedsha.suheb2020@vitstudent.ac.in>
2024-03-16 21:16:55 +0530

    array

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 
 //basics
-const myname="suheb"
+// const myname="suheb"
 
-let a=5
+// let a=5
 
-console.log(myname)  //cannot be changed
-console.log(a)
+// console.log(myname)  //cannot be changed
+// console.log(a)
 
-a=999
+// a=999
 
-console.log(a)
+// console.log(a)
 
 //datatypes
-let mynum;
-console.log(mynum)
+// let mynum;
+// console.log(mynum)
 
-let boolvalue=true
-let nullvalue=null
+// let boolvalue=true
+// let nullvalue=null
 
-console.log(boolvalue)
-console.log(nullvalue)
+// console.log(boolvalue)
+// console.log(nullvalue)
+
+//array
+
+let array=[1,2,3,4,5,6,7,8]
+
+console.log(array.length)
+console.log(array[0])
+console.log(array[1])
+console.log(array[array.length-1])
